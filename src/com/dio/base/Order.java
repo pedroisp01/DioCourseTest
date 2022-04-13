14c8407a487f3ee8735c1adeb4f427e40cdfeab6
Pedro Azevedo <pedro.azvd01@gmail.com>
2022-04-12 21:34:38 -0300

    First Commit

---
+++ src/com/dio/base/Order.java
@@ -0,0 +1,22 @@
+package com.dio.base;
+
+import java.math.BigDecimal;
+
+/**
+ * @author Pedro Azevedo
+ * @version 1.0.0
+ * @see BigDecimal
+ */
+
+public class Order {
+
+    private final String code;
+    //private final BigDecimal totalValue;
+
+    private String[] items;
+
+    public Order(String code) {
+        this.code = code;
+    }
+
+}
